56637620349d279a273366cf86e703bebe81c379
Michael Montesino <mmontesino@live.com>
2017-09-03 19:23:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ function happyHolidays() {
 function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 }
-function happyHolidayTo(person, holiday) {
+function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
 function holidayCountdown(holiday, days) {
   return `It\'s ${days} days until ${holiday}!`
 }
+
